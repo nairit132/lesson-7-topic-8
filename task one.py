836f90f82e582259210a9b18364bbd4ab836cbe1
nairit132 <nairithmajmundar@gmail.com>
2025-08-07 17:41:00 +0100

    Add files via upload

---
+++ task one.py
@@ -0,0 +1,12 @@
+tuplex = ("tuple",False,3.2,1)
+print(tuple)
+tuplex =(4,6,3,2,8,3,1)
+print(tuplex)
+tuplex = tuplex + (9,)
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+tuplex = (1,5,4,6,3,3,7,3,2,5,8,9,0,3)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
